e3664ac94a185e3b5b6b22bd3b470ef3b4705922
Animesh Srivastava <98380129+Animesh0203@users.noreply.github.com>
2024-07-14 18:16:40 +0530

    Create consume.py

---
+++ consumer/consume.py
@@ -0,0 +1,29 @@
+from kafka import KafkaConsumer
+import csv
+import json
+
+# Initialize Kafka consumer
+consumer = KafkaConsumer(
+    'temp',
+    'humidity',
+    bootstrap_servers='YOUR_KAFKA_SERVER:9092',
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='sensor_group',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+# Define CSV file to store the data
+csv_file = 'sensor_data.csv'
+
+# Open the CSV file for writing
+with open(csv_file, 'w', newline='') as file:
+    writer = csv.writer(file)
+    # Write the header
+    writer.writerow(['timestamp', 'sensor1', 'sensor2'])
+
+    # Consume messages from Kafka
+    for message in consumer:
+        data = message.value
+        # Write the data to CSV
+        writer.writerow([data['timestamp'], data['sensor1'], data['sensor2']])
